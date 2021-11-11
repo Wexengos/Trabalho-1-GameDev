4bd05fb8bbd53a2ce44ed6bbdc742d956e95888b
Wexengos <gnos2007@gmail.com>
2021-11-11 11:42:49 -0300

    Início da implementação de plataformas + colisão e pulo

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    bool facingRight = true;
+    public bool facingRight = true;  //O personagem está olhando para a direita?
+
+    public float jumpHeight;
+    private bool isGrounded;  //O personagem está num chão?
+
     public Animator animator;
-    
-    float isFacingRight(float xScale) {                         //Verifica: se o personagem estiver se movendo para um lado e
-        if(Input.GetAxis("Horizontal") < 0 && facingRight) {    //não estiver olhando para este, o lado para o qual ele está
+
+    float isFacingRight(float xScale)
+    {                         //Verifica: se o personagem estiver se movendo para um lado e
+        if (Input.GetAxis("Horizontal") < 0 && facingRight)
+        {    //não estiver olhando para este, o lado para o qual ele está
             facingRight = false;                                //virado se altera. 
             return -xScale;
         }
-        else if(Input.GetAxis("Horizontal") > 0 && !facingRight) {
+        else if (Input.GetAxis("Horizontal") > 0 && !facingRight)
+        {
             facingRight = true;
             return -xScale;
         }
         else
             return xScale;
     }
+
     // Update is called once per frame
     void Update()
     {
-    
+
         float xScale = transform.localScale.x;
         float yScale = transform.localScale.y;
 
@@ -34,5 +42,24 @@ public class BasicMovement : MonoBehaviour
         transform.position = transform.position + horizontal * Time.deltaTime * 5;            //"Time.deltaTime" deixa o movimento "mais liso",
                                                                                               //uma vez que o script roda a cada Update().
         transform.localScale = new Vector2(side.x, yScale);  //espelhamento do personagem no movimento
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpHeight), ForceMode2D.Force);  //checa se o personagem está no chão,
+                                                                                                     //se sim, o botão de espaço o faz pular.
+        }
+    }
+                             //Há uma caixa de colisão com um 'Is Trigger' que envolve o jogador, que é ativado toda vez que este colide.
+    void OnTriggerEnter2D()  
+    {                        
+        isGrounded = true;   //Se o jogador toca o chão, ele está no chão.
+    }
+    void OnTriggerStay2D()
+    {
+        isGrounded = true;   //Se ele permanece no chão, obviamente está no chão.
+    }
+    void OnTriggerExit2D()
+    {
+        isGrounded = false;  //Se ele deixa o chão, não pode pular, pois já está caindo/pulando.
     }
 }
