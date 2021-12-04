5de61faee9f6ec91e48be6b77ff18565e5e73bef
Guilherme Marques <guilhermemarques1@ice.ufjf.br>
2021-12-04 14:14:05 -0300

    Rat Camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        transform.position = temp;
+    }
+}
